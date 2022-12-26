c3d95c8a4696f88c4d4aa792d13254dfa84d9f85
Dohyun Chung <pastapeter@naver.com>
2022-12-26 22:07:40 +0900

    [add] project setting

---
+++ Keyneez/Keyneez/Application/SceneDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  SceneDelegate.swift
+//  Keyneez
+//
+//  Created by Jung peter on 12/26/22.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  
+  var window: UIWindow?
+  
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    
+    if let windowScene = scene as? UIWindowScene {
+      
+      let window = UIWindow(windowScene: windowScene)
+      window.overrideUserInterfaceStyle = UIUserInterfaceStyle.light
+      
+      let tabbarVC = KeyneezTabbarController()
+      window.rootViewController = tabbarVC
+      window.makeKeyAndVisible()
+      self.window = window
+    }
+  }
+  
+  func sceneDidDisconnect(_ scene: UIScene) {
+  }
+  
+  func sceneDidBecomeActive(_ scene: UIScene) {
+  }
+  
+  func sceneWillResignActive(_ scene: UIScene) {
+  }
+  
+  func sceneWillEnterForeground(_ scene: UIScene) {
+  }
+  
+  func sceneDidEnterBackground(_ scene: UIScene) {
+  }
+  
+  
+}
+
---
+++ Keyneez/Keyneez/Tab/KeyneezTabbarController.swift
@@ -0,0 +1,19 @@
+//
+//  KeyneezTabbarController.swift
+//  Keyneez
+//
+//  Created by Jung peter on 12/26/22.
+//
+
+import UIKit
+
+class KeyneezTabbarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    self.view.backgroundColor = .red
+    
+    // Do any additional setup after loading the view.
+  }
+
+}
