96cd95bcc526005b76f50ded6d0c4a1f4d2167bc
Dohyun Chung <pastapeter@naver.com>
2022-12-30 00:11:33 +0900

    [Add]#1 Project Setting SwiftLint 추가

--- Keyneez/Keyneez/Tab/KeyneezTabbarController.swift
+++ Keyneez/Keyneez/Tab/KeyneezTabbarController.swift
@@ -11,11 +11,10 @@ class KeyneezTabbarController: UITabBarController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    var image = UIImage(named: "Hello")!
     self.view.backgroundColor = .red
     
     // Do any additional setup after loading the view.
   }
 
 }
-
-
