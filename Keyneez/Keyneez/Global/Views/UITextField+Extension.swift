5cc6347de6f9429cde94ff58e39b0f716c3bdd2f
kpk0616 <kpk0616@naver.com>
2023-01-03 04:18:23 +0900

    [Fix] #4 - Color 옵셔널체이닝 제거

--- Keyneez/Keyneez/Global/Views/UITextField+Extension.swift
+++ Keyneez/Keyneez/Global/Views/UITextField+Extension.swift
@@ -8,40 +8,6 @@
 import UIKit
 import Then
 
-enum KeyneezTextFieldFactory: Buildable {
-
-    typealias ViewType = UITextField
-
-    enum TextfieldBorderStyle {
-      case underline
-      case underlineIcon
-    }
-
-    case formStyleTextfield(placeholder: String, borderStyle: TextfieldBorderStyle)
-
-    func build(_ config: ((ViewType) -> Void)? = nil) -> ViewType {
-        switch self {
-        case .formStyleTextfield(let placeholder, let borderStyle):
-            var textfield =  makeTextField(placeholer: placeholder, borderstyle: borderStyle)
-            if let config = config {
-                textfield = textfield.then(config)
-            }
-            return textfield
-        }
-    }
-
-    private func makeTextField(placeholer: String, borderstyle: TextfieldBorderStyle) -> ViewType {
-        return UITextField().then {
-            $0.placeholder = placeholer
-            $0.textColor = .lightGray
-            if borderstyle == .underline {
-                $0.underlineStyle()
-            } else if borderstyle == .round {
-                $0.borderStyle = .roundedRect
-            }
-        }
-    }
-
 extension UITextField {
   
   fileprivate class Constant {
