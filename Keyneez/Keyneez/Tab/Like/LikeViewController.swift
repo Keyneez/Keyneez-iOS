2c68868e841e7f370a260dfa2ddfda3e0f4b0edd
kpk0616 <kpk0616@naver.com>
2023-01-02 20:25:03 +0900

    [Add] #4 - LikeViewController 추가

---
+++ Keyneez/Keyneez/Tab/Like/LikeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LikeViewController.swift
+//  Keyneez
+//
+//  Created by 박의서 on 2023/01/02.
+//
+
+import UIKit
+
+class LikeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
