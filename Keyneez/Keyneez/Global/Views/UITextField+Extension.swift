c2ae55b61e2901820f56e09664daa46b8e3b6a92
DOHYUN CHUNG <69891604+pastapeter@users.noreply.github.com>
2023-01-03 02:57:40 +0900

    Feat/#16 (#17)
    * [Add] borderWidthAndBorderColor 함수 추가
    
    * [Add] Color 강제 바인딩
    
    * [Feat] TextField custom
    
    * [Add] proj file

--- Keyneez/Keyneez/Global/Views/UITextField+Extension.swift
+++ Keyneez/Keyneez/Global/Views/UITextField+Extension.swift
@@ -8,38 +8,30 @@
 import UIKit
 import Then
 
-enum KeyneezTextFieldFactory: Buildable {
-
-    typealias ViewType = UITextField
-    
-    enum TextfieldBorderStyle {
-      case underline
-      case underlineIcon
-    }
-
-    case formStyleTextfield(placeholder: String, borderStyle: TextfieldBorderStyle)
-
-    func build(_ config: ((ViewType) -> Void)? = nil) -> ViewType {
-        switch self {
-        case .formStyleTextfield(let placeholder, let borderStyle):
-            var textfield =  makeTextField(placeholer: placeholder, borderstyle: borderStyle)
-            if let config = config {
-                textfield = textfield.then(config)
-            }
-            return textfield
-        }
-    }
-    
-    private func makeTextField(placeholer: String, borderstyle: TextfieldBorderStyle) -> ViewType {
-        return UITextField().then {
-            $0.placeholder = placeholer
-            $0.textColor = .lightGray
-            if borderstyle == .underline {
-                $0.underlineStyle()
-            } else if borderstyle == .round {
-                $0.borderStyle = .roundedRect
-            }
-        }
-    }
-    
+extension UITextField {
+  
+  fileprivate class Constant {
+    static let textFieldHeight: CGFloat = 48
+    static let padding: CGFloat = 24
+  }
+  
+  func underlineStyle(textColor: UIColor = .gray900,
+                      borderColor: UIColor = .gray400,
+                      padding: CGFloat = Constant.padding,
+                      height: CGFloat = Constant.textFieldHeight) {
+    self.borderStyle = .none
+    let bottomLine = CALayer()
+    bottomLine.frame = CGRect(x: 0,
+                              y: height,
+                              width: UIScreen.main.bounds.width - (2 * padding),
+                              height: 1)
+    bottomLine.backgroundColor = borderColor.cgColor
+    self.layer.addSublayer(bottomLine)
+    self.textColor = textColor
+  }
+  
+  func setPlaceholderColorAndFont(_ placeholderColor: UIColor, font: UIFont) {
+    attributedPlaceholder = NSAttributedString(string: placeholder ?? "", attributes: [NSAttributedString.Key.foregroundColor : placeholderColor, .font: font].compactMapValues{$0})
+  }
+  
 }
