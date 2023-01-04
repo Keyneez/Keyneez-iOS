f371dca86b986be13b44a632499284627a432535
DOHYUN CHUNG <69891604+pastapeter@users.noreply.github.com>
2023-01-04 16:43:05 +0900

    Feature/#2 네비게이션뷰 구현 (#19)
    * [Add] borderWidthAndBorderColor 함수 추가
    
    * [Add] Color 강제 바인딩
    
    * [Feat] TextField custom
    
    * [Add] proj file
    
    * [Add] TextField UnderlinePadding 변경할수있는 코드
    
    * [Style] Lint 맞추기
    
    * [Add] NiblessViewController 추가
    
    required init? 을 사용하지 않아도 초기화 가능
    
    * [Feat] NavigationBarView
    
    * [Fix] NavigationViewController 내장 barview 숨기기
    
    * [Chore] project file
    
    * [Chore] Lint Warning 제거

---
+++ Keyneez/Keyneez/Global/Views/Nibless/NiblessView.swift
@@ -0,0 +1,24 @@
+//
+//  NiblessView.swift
+//  Keyneez
+//
+//  Created by Jung peter on 1/3/23.
+//
+
+import UIKit
+
+class NiblessView: UIView {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  @available(*, unavailable,
+              message: "We do not support Storyboard"
+  )
+  
+  required init?(coder aDecoder: NSCoder) {
+      fatalError("We do not support Storyboard")
+  }
+  
+}
