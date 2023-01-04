6bd8f06557fcf6ad37c812c606f17825b0ce14c0
kpk0616 <kpk0616@naver.com>
2023-01-04 17:01:02 +0900

    [Add] #9 - HomeTabView 생성

--- Keyneez/Keyneez/Tab/Home/HomeViewController.swift
+++ Keyneez/Keyneez/Tab/Home/HomeViewController.swift
@@ -7,23 +7,9 @@
 
 import UIKit
 
-final class HomeViewController: UIViewController {
+final class HomeViewController: NiblessViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .gray500
-    
-    // Do any additional setup after loading the view.
   }
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
 }
