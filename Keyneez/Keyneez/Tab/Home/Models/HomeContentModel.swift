6d8ad3e1731dccbd1f390d220a4b4c0f72f10ef4
kpk0616 <kpk0616@naver.com>
2023-01-06 18:22:10 +0900

    [Add] #9 - HomeContentModel 추가

---
+++ Keyneez/Keyneez/Tab/Home/Models/HomeContentModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeContentModel.swift
+//  Keyneez
+//
+//  Created by 박의서 on 2023/01/06.
+//
+
+import Foundation
+
+struct HomeContentModel {
+  let contentImage: String
+  let start_at: String
+  let end_at: String
+  let contentTitle: String
+  let categoty: [String]
+  let liked: Bool
+}
