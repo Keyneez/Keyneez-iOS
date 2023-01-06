6bc7480a9bfbf83e2a71577c5c91740bf8546942
kpk0616 <kpk0616@naver.com>
2023-01-06 17:22:18 +0900

    [Add] #9 - HomeInfoCollectionView 추가

---
+++ Keyneez/Keyneez/Tab/Home/Views/HomeInfoCollectionView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeInfoCollectionView.swift
+//  Keyneez
+//
+//  Created by 박의서 on 2023/01/06.
+//
+
+import UIKit
+
+class HomeInfoCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
