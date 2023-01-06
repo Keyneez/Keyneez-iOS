6bbf5700f33774bc9993785b7cc66504875c4058
Dohyun Chung <pastapeter@naver.com>
2023-01-06 18:47:25 +0900

    [Add] IDCardContentView로 contentview 변경

--- Keyneez/Keyneez/Tab/ID/IDViewController.swift
+++ Keyneez/Keyneez/Tab/ID/IDViewController.swift
@@ -11,9 +11,12 @@ final class IDViewController: NiblessViewController, NavigationBarProtocol {
   
   // 커스텀 네비게이션 뷰 생성
   var navigationView: UIView = NavigationViewBuilder(barViews: [.logo(color: .white), .flexibleBox]).build()
+  var actions = IDContentActions()
   
   // 컨텐츠 뷰 생성
-  var contentView: UIView = IDContentView()
+  lazy var contentView: UIView = IDCardContentView(frame: .zero, actions: actions)
+  
+  private lazy var customNavigationDelegate = CustomNavigationManager()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -29,4 +32,14 @@ extension IDViewController {
     view.backgroundColor = .gray800
   }
   
+  private func makeAction() -> UIAction {
+    return UIAction(handler: { [unowned self] _ in
+      let idDetailVC = IDDetailViewController()
+      self.customNavigationDelegate.direction = .bottom
+      self.customNavigationDelegate.height = 348
+      idDetailVC.transitioningDelegate = self.customNavigationDelegate
+      idDetailVC.modalPresentationStyle = .custom
+      self.present(idDetailVC, animated: true, completion: nil)
+    })
+  }
 }
