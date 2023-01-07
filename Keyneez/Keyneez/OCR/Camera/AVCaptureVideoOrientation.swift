e59c941774b90565a17e52319c44a3f8c321adef
Dohyun Chung <pastapeter@naver.com>
2023-01-08 04:37:27 +0900

    [Add] AVCaptureVideoOrientation, DiscoverySession 파일 추가

---
+++ Keyneez/Keyneez/OCR/Camera/AVCaptureVideoOrientation.swift
@@ -0,0 +1,31 @@
+//
+//  AVCaptureSession.swift
+//  Keyneez
+//
+//  Created by Jung peter on 1/8/23.
+//
+
+import AVFoundation
+import UIKit
+
+extension AVCaptureVideoOrientation {
+    init?(deviceOrientation: UIDeviceOrientation) {
+        switch deviceOrientation {
+        case .portrait: self = .portrait
+        case .portraitUpsideDown: self = .portraitUpsideDown
+        case .landscapeLeft: self = .landscapeRight
+        case .landscapeRight: self = .landscapeLeft
+        default: return nil
+        }
+    }
+    
+    init?(interfaceOrientation: UIInterfaceOrientation) {
+        switch interfaceOrientation {
+        case .portrait: self = .portrait
+        case .portraitUpsideDown: self = .portraitUpsideDown
+        case .landscapeLeft: self = .landscapeLeft
+        case .landscapeRight: self = .landscapeRight
+        default: return nil
+        }
+    }
+}
