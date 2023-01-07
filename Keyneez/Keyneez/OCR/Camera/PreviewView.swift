8b41d8120f5bb883ce80d1fb14ec9d932a38b336
Dohyun Chung <pastapeter@naver.com>
2023-01-08 04:38:48 +0900

    [Add] PreviewView Masking

--- Keyneez/Keyneez/OCR/Camera/PreviewView.swift
+++ Keyneez/Keyneez/OCR/Camera/PreviewView.swift
@@ -9,6 +9,7 @@ import UIKit
 import AVFoundation
 
 final class PreviewView: UIView {
+  
   var videoPreviewLayer: AVCaptureVideoPreviewLayer {
       guard let layer = layer as? AVCaptureVideoPreviewLayer else {
           fatalError("Expected `AVCaptureVideoPreviewLayer` type for layer. Check PreviewView.layerClass implementation.")
@@ -28,4 +29,48 @@ final class PreviewView: UIView {
   override class var layerClass: AnyClass {
       return AVCaptureVideoPreviewLayer.self
   }
+  
+  private var maskLayer = CAShapeLayer()
+  private var regionOfInterestOutline = CAShapeLayer()
+  private lazy var regionOfInterest = CGRect(x: CGFloat((UIScreen.main.bounds.size.width - 327) / 2), y: 312, width: 327, height: 200)
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    commonInit()
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    
+    commonInit()
+  }
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    
+    let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+    path.append(UIBezierPath(roundedRect: regionOfInterest, cornerRadius: 12))
+    path.usesEvenOddFillRule = true
+    maskLayer.path = path.cgPath
+    regionOfInterestOutline.path = CGPath(roundedRect: regionOfInterest, cornerWidth: 12, cornerHeight: 12, transform: nil)
+  }
+  
+  func commonInit() {
+    
+    videoPreviewLayer.videoGravity = .resizeAspectFill
+    
+    maskLayer.fillRule = .evenOdd
+    maskLayer.fillColor = UIColor.black.cgColor
+    maskLayer.opacity = 0.8
+    layer.addSublayer(maskLayer)
+    
+    
+    regionOfInterestOutline.lineWidth = 3
+    regionOfInterestOutline.path = UIBezierPath(roundedRect: regionOfInterest, cornerRadius: 12).cgPath
+    regionOfInterestOutline.fillColor = UIColor.clear.cgColor
+    regionOfInterestOutline.strokeColor = UIColor.white.cgColor
+    layer.addSublayer(regionOfInterestOutline)
+    
+  }
+  
 }
