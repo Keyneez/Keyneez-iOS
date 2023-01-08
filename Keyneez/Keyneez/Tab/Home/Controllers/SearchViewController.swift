73933c7fdbb1763636e57574fc1a58e25e2dd358
kpk0616 <kpk0616@naver.com>
2023-01-09 00:45:57 +0900

    [Add] #15 - SearchViewController 추가

---
+++ Keyneez/Keyneez/Tab/Home/Controllers/SearchViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SearchViewController.swift
+//  Keyneez
+//
+//  Created by 박의서 on 2023/01/09.
+//
+
+import UIKit
+
+final class SearchViewController: NiblessViewController, NavigationBarProtocol {
+//  var navigationView: UIView = NavigationViewBuilder(barViews: [.button(with: "ic_arrowback"), .textfield(configure: (placeholder: "검색", completion: { _ in print("hi") })]).build()
+  lazy var navigationView: UIView = NavigationViewBuilder(barViews: [.logo(color: .black), .flexibleBox, .iconButton(with: searchButton)]).build()
+  private lazy var searchButton: UIButton = .init(primaryAction: didSearch).then {
+    $0.setBackgroundImage(UIImage(named: "ic_search"), for: .normal)
+  }
+  private var didSearch: UIAction = .init(handler: { _ in print("hi") })
+  
+  // 컨텐츠 뷰 생성
+  var contentView: UIView = UIView()
+//
+//  override func viewDidLoad() {
+//    super.viewDidLoad()
+//    addNavigationViewToSubview()
+//  }
+    
+}
