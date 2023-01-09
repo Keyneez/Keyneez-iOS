789b57c5e82e5f53e9e75272c15840619d9176c9
kpk0616 <kpk0616@naver.com>
2023-01-10 05:23:47 +0900

    [Del] #15 - 검색뷰 셀 타이틀 함수 삭제

--- Keyneez/Keyneez/Tab/Home/Views/HomeSearchCollectionViewCell.swift
+++ Keyneez/Keyneez/Tab/Home/Views/HomeSearchCollectionViewCell.swift
@@ -81,44 +81,5 @@ extension HomeSearchCollectionViewCell {
   private func setDateLabel(model: HomeContentModel) -> String {
     if model.startAt.isEmpty || model.endAt.isEmpty { return "2023 ~ " }
     return getDate(fullDate: model.startAt) + " ~ " + getDate(fullDate: model.endAt)
-  }
   
-//  private func setTitleLabel() -> String {
-//    let title = "청소년 영화관 할인" // 나 새콤 - 달콤 좋아아주 ["나", "새콤", "달콤", "좋아아주"]
-//    let separeteString = title.components(separatedBy: " ")
-//    var results: [String] = []
-//    var resultString = ""
-//    for words in separeteString {
-//      if separeteString.lastIndex(of: words) == separeteString.count - 1 {
-//        results.append(resultString)
-//        return title
-//      }
-//      if resultString.count < 6 {
-//        if resultString.count + words.count <= 6 {
-//          resultString += words
-//          resultString += " "
-//        } else {
-//          results.append(resultString)
-//          resultString = words
-//        }
-//        // 붙인 글자가 7자 이상이면 넘어가고, 6자 이하면 붙이기
-//      } else {
-//        if words.count > 6 {
-//          let startIndex = words.index(words.startIndex, offsetBy: 0)
-//          let maxIndex = words.index(words.startIndex, offsetBy: 6)
-//          let nextIndex = words.index(words.startIndex, offsetBy: 7)
-//          results.append(String(words[startIndex ... maxIndex]))
-//          resultString += String(words[nextIndex ... words.endIndex])
-//        }
-//        // 6번째까지 잘라서 results 에 넣고, 그 뒤를 resultString으로 넘기기
-//      }
-//      print(results)
-//      print(resultString)
-//      // index 넘어가면 글자수 하나 + -> 6자리까지 허용.. V
-//      // results 에 계속 붙여. 인덱스 넘어갈 때마다 results 글자수 검사 v
-//      // results 가 7자리 이상이면 새로운 문자열 만들기
-//      // resultString 사이 \n 추가
-//    }
-//    return title
-//  }
 }
