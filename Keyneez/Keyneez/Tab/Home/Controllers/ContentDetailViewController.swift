af75d5563d22599bfe1365a411c38747064bf122
kpk0616 <kpk0616@naver.com>
2023-01-09 20:32:52 +0900

    [Chore] #15 - ContentDetailViewController 네비로 변경

--- Keyneez/Keyneez/Tab/Home/Controllers/ContentDetailViewController.swift
+++ Keyneez/Keyneez/Tab/Home/Controllers/ContentDetailViewController.swift
@@ -7,23 +7,17 @@
 
 import UIKit
 
-final class ContentDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+final class ContentDetailViewController: NiblessViewController, NavigationBarProtocol {
+  
+  // 커스텀 네비게이션 뷰 생성
+  var navigationView: UIView
+  
+  // 컨텐츠 뷰 생성
+  var contentView: UIView
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    addNavigationViewToSubview()
+  }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
