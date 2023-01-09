a3ed6688123b77ac0333a3868f3aa6f7158558f6
kpk0616 <kpk0616@naver.com>
2023-01-09 20:53:06 +0900

    [Feat] #15 - 인포상세뷰 네비 구현

--- Keyneez/Keyneez/Tab/Home/Controllers/ContentDetailViewController.swift
+++ Keyneez/Keyneez/Tab/Home/Controllers/ContentDetailViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 final class ContentDetailViewController: NiblessViewController, NavigationBarProtocol {
   
-  // 커스텀 네비게이션 뷰 생성
-  var navigationView: UIView
-  
-  // 컨텐츠 뷰 생성
-  var contentView: UIView
+  lazy var navigationView: UIView = NavigationViewBuilder(barViews: [.iconButton(with: backButton), .flexibleBox, .iconButton(with: shareButton), .sizedBox(width: 16), .iconButton(with: likeButton)]).build()
+  private lazy var backButton: UIButton = .init(primaryAction: touchUpBackButton).then {
+    $0.setBackgroundImage(UIImage(named: "ic_arrowback_search"), for: .normal)
+  }
+  private lazy var shareButton: UIButton = .init(primaryAction: touchUpShareButton).then {
+    $0.setBackgroundImage(UIImage(named: "ic_share3"), for: .normal)
+  }
+  private lazy var likeButton: UIButton = .init(primaryAction: touchUpLikeButton).then {
+    $0.setBackgroundImage(UIImage(named: "Property 1=line"), for: .normal)
+  }
+  private lazy var touchUpBackButton: UIAction = .init(handler: { _ in
+    self.navigationController?.popViewController(animated: true)
+  })
+  private lazy var touchUpShareButton: UIAction = .init(handler: { _ in
+    print("touch up ShareButton")
+  })
+  private lazy var touchUpLikeButton: UIAction = .init(handler: { _ in
+    print("touch up LikeButton")
+  })
+  var contentView: UIView = .init()
   
   override func viewDidLoad() {
     super.viewDidLoad()
