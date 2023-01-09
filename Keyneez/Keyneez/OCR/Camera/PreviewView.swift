facfb88b51dff9fb3b0eca1c121299e235bd33e3
Dohyun Chung <pastapeter@naver.com>
2023-01-10 02:34:41 +0900

    [Style] Constant화

--- Keyneez/Keyneez/OCR/Camera/PreviewView.swift
+++ Keyneez/Keyneez/OCR/Camera/PreviewView.swift
@@ -8,6 +8,19 @@
 import UIKit
 import AVFoundation
 
+private struct Constant {
+  static let cardLargeSize: CGFloat = 327
+  static let cardSmallSize: CGFloat = 200
+  static let cornerRadius: CGFloat = 12
+  static let verticalTitleGap: CGFloat = 32
+  static let horizontalTitleGap: CGFloat = 40
+  static let verticalCardGap: CGFloat = 251
+  static let horizontalCardGap: CGFloat = 312
+  static let autoDescription = "학생증/청소년증을\n평평하고 그늘진 곳에 놓아주세요"
+  static let manualDescription = "준비가 끝나면 촬영 버튼을 눌러주세요"
+  static let guideTitle = "신분증 앞면을\n사각형 안에 맞춰주세요."
+}
+
 final class PreviewView: NiblessView {
   
   var videoPreviewLayer: AVCaptureVideoPreviewLayer {
@@ -31,7 +44,7 @@ final class PreviewView: NiblessView {
   }
   
   private lazy var guideTitleLabel: UILabel = .init().then {
-    $0.text = "신분증 앞면을\n사각형 안에 맞춰주세요."
+    $0.text = Constant.guideTitle
     $0.font = .font(.pretendardBold, ofSize: 24)
     $0.textColor = .gray050
     $0.textAlignment = .center
@@ -39,7 +52,7 @@ final class PreviewView: NiblessView {
   }
   
   private lazy var guideDescriptionLabel: UILabel = .init().then {
-    $0.text = "학생증/청소년증을\n평평하고 그늘진 곳에 놓아주세요"
+    $0.text = Constant.autoDescription
     $0.textColor = .gray500
     $0.font = .font(.pretendardMedium, ofSize: 18)
     $0.textAlignment = .center
@@ -63,10 +76,10 @@ final class PreviewView: NiblessView {
     self.regionOfInterest = self.regionOfInterestView.frame
     
     let path = UIBezierPath(rect: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
-    path.append(UIBezierPath(roundedRect: regionOfInterest, cornerRadius: 12))
+    path.append(UIBezierPath(roundedRect: regionOfInterest, cornerRadius: Constant.cornerRadius))
     path.usesEvenOddFillRule = true
     maskLayer.path = path.cgPath
-    regionOfInterestOutline.path = CGPath(roundedRect: regionOfInterest, cornerWidth: 12, cornerHeight: 12, transform: nil)
+    regionOfInterestOutline.path = CGPath(roundedRect: regionOfInterest, cornerWidth: Constant.cornerRadius, cornerHeight: Constant.cornerRadius, transform: nil)
     
   }
   
@@ -74,19 +87,19 @@ final class PreviewView: NiblessView {
   internal func changeToVertical() {
       regionOfInterestView.snp.remakeConstraints {
         $0.centerX.equalToSuperview()
-        $0.height.equalTo(327)
-        $0.width.equalTo(200)
-        $0.top.equalToSuperview().inset(251)
+        $0.height.equalTo(Constant.cardLargeSize)
+        $0.width.equalTo(Constant.cardSmallSize)
+        $0.top.equalToSuperview().inset(Constant.verticalCardGap)
       }
       
       guideTitleLabel.snp.remakeConstraints {
         $0.centerX.equalToSuperview()
-        $0.bottom.equalTo(regionOfInterestView.snp.top).offset(-32)
+        $0.bottom.equalTo(regionOfInterestView.snp.top).offset(-Constant.verticalTitleGap)
       }
       
       guideDescriptionLabel.snp.remakeConstraints {
         $0.centerX.equalToSuperview()
-        $0.top.equalTo(regionOfInterestView.snp.bottom).offset(32)
+        $0.top.equalTo(regionOfInterestView.snp.bottom).offset(Constant.verticalTitleGap)
       }
    
   }
@@ -103,19 +116,19 @@ final class PreviewView: NiblessView {
   internal func changeToHorizontal() {
     regionOfInterestView.snp.remakeConstraints {
       $0.centerX.equalToSuperview()
-      $0.height.equalTo(200)
-      $0.width.equalTo(327)
-      $0.top.equalToSuperview().inset(312)
+      $0.height.equalTo(Constant.cardSmallSize)
+      $0.width.equalTo(Constant.cardLargeSize)
+      $0.top.equalToSuperview().inset(Constant.horizontalCardGap)
     }
     
     guideTitleLabel.snp.remakeConstraints {
       $0.centerX.equalToSuperview()
-      $0.bottom.equalTo(regionOfInterestView.snp.top).offset(-40)
+      $0.bottom.equalTo(regionOfInterestView.snp.top).offset(-Constant.horizontalTitleGap)
     }
     
     guideDescriptionLabel.snp.remakeConstraints {
       $0.centerX.equalToSuperview()
-      $0.top.equalTo(regionOfInterestView.snp.bottom).offset(40)
+      $0.top.equalTo(regionOfInterestView.snp.bottom).offset(Constant.horizontalTitleGap)
     }
   
   }
@@ -136,7 +149,7 @@ extension PreviewView {
     layer.addSublayer(maskLayer)
     
     regionOfInterestOutline.lineWidth = 3
-    regionOfInterestOutline.path = UIBezierPath(roundedRect: regionOfInterest, cornerRadius: 12).cgPath
+    regionOfInterestOutline.path = UIBezierPath(roundedRect: regionOfInterest, cornerRadius: Constant.cornerRadius).cgPath
     regionOfInterestOutline.fillColor = UIColor.clear.cgColor
     regionOfInterestOutline.strokeColor = UIColor.white.cgColor
     layer.addSublayer(regionOfInterestOutline)
@@ -145,9 +158,9 @@ extension PreviewView {
   private func didCaptureModeChangeWhenHorizontal(with mode: CaptureMode) {
     switch mode {
     case .manual:
-      guideDescriptionLabel.text = "준비가 끝나면 촬영 버튼을 눌러주세요"
+      guideDescriptionLabel.text = Constant.manualDescription
     case .auto:
-      guideDescriptionLabel.text =  "학생증/청소년증을\n평평하고 그늘진 곳에 놓아주세요"
+      guideDescriptionLabel.text =  Constant.autoDescription
     }
   }
   
@@ -156,7 +169,7 @@ extension PreviewView {
     case .manual:
       guideDescriptionLabel.text = nil
     case .auto:
-      guideDescriptionLabel.text =  "학생증/청소년증을\n평평하고 그늘진 곳에 놓아주세요"
+      guideDescriptionLabel.text =  Constant.autoDescription
     }
   }
   
@@ -168,19 +181,19 @@ extension PreviewView {
     
     regionOfInterestView.snp.makeConstraints {
       $0.centerX.equalToSuperview()
-      $0.height.equalTo(200)
-      $0.width.equalTo(327)
-      $0.top.equalToSuperview().inset(312)
+      $0.height.equalTo(Constant.cardSmallSize)
+      $0.width.equalTo(Constant.cardLargeSize)
+      $0.top.equalToSuperview().inset(Constant.horizontalCardGap)
     }
     
     guideTitleLabel.snp.makeConstraints {
       $0.centerX.equalToSuperview()
-      $0.bottom.equalTo(regionOfInterestView.snp.top).offset(-40)
+      $0.bottom.equalTo(regionOfInterestView.snp.top).offset(-Constant.horizontalTitleGap)
     }
     
     guideDescriptionLabel.snp.makeConstraints {
       $0.centerX.equalToSuperview()
-      $0.top.equalTo(regionOfInterestView.snp.bottom).offset(40)
+      $0.top.equalTo(regionOfInterestView.snp.bottom).offset(Constant.horizontalTitleGap)
     }
   }
 }
