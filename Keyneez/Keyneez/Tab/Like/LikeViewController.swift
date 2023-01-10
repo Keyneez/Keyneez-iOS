ce1b7cb5031e74f4e925aba86f174992c378bbf4
kpk0616 <kpk0616@naver.com>
2023-01-10 23:17:54 +0900

    [Feat] #15 - 상단 바 저장 버튼 크기 지정

--- Keyneez/Keyneez/Tab/Like/LikeViewController.swift
+++ Keyneez/Keyneez/Tab/Like/LikeViewController.swift
@@ -7,22 +7,29 @@
 
 import UIKit
 
-final class LikeViewController: UIViewController {
+final class LikeViewController: NiblessViewController, NavigationBarProtocol {
+  lazy var navigationView: UIView = NavigationViewBuilder(barViews: [.button(with: myLikeButton), .flexibleBox, .iconButton(with: editButton)]).build()
+  private lazy var myLikeButton: UIButton = .init(primaryAction: touchUpMyLikeButton).then {
+    $0.setTitle("저장", for: .normal)
+    $0.setTitleColor(.gray900, for: .normal)
+    $0.titleLabel?.font = .font(.pretendardBold, ofSize: 20)
+    $0.frame.size.width = 35
+    $0.frame.size.height = 24
+  }
+  private lazy var touchUpMyLikeButton: UIAction = .init(handler: { _ in
+    print("touch up my like button")
+  })
+  private lazy var editButton: UIButton = .init(primaryAction: touchUpEditButton).then {
+    $0.setBackgroundImage(UIImage(named: "ic_edit"), for: .normal)
+  }
+  private lazy var touchUpEditButton: UIAction = .init { _ in
+    print("touchUpEditButton")
+  }
+  var contentView: UIView = UIView()
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Do any additional setup after loading the view.
+    view.backgroundColor = .white
+    addNavigationViewToSubview()
   }
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
 }
