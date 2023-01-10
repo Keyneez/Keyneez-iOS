14358d9b528ce76a77262be09467d2ed54c2aabd
kpk0616 <kpk0616@naver.com>
2023-01-10 18:15:52 +0900

    [Feat] #15 - HomeSearchModel 작성

--- Keyneez/Keyneez/Tab/Home/Models/HomeSearchModel.swift
+++ Keyneez/Keyneez/Tab/Home/Models/HomeSearchModel.swift
@@ -7,6 +7,27 @@
 
 import UIKit
 
-final class HomeSearchModel: UIView {
-
+final class HomeSearchModel {
+  let contentKey = UUID()
+  let contentTitle: String
+  let startAt: String
+  let endAt: String
+  let isLiked: Bool
+  
+  init(contentTitle: String, startAt: String, endAt: String, isLiked: Bool) {
+    self.contentTitle = contentTitle
+    self.startAt = startAt
+    self.endAt = endAt
+    self.isLiked = isLiked
+  }
 }
+
+var homeSearchList: [HomeSearchModel] = [
+  HomeSearchModel(contentTitle: "청소년 미술관 할인", startAt: "", endAt: "", isLiked: false),
+  HomeSearchModel(contentTitle: "여행? 바로 가자", startAt: "20221124", endAt: "20221231", isLiked: true),
+  HomeSearchModel(contentTitle: "청소년 영화관 할인", startAt: "20221124", endAt: "20221231", isLiked: true),
+  HomeSearchModel(contentTitle: "청소년 영화관 할인", startAt: "", endAt: "", isLiked: false),
+  HomeSearchModel(contentTitle: "청소년 영화관 할인", startAt: "", endAt: "", isLiked: false),
+  HomeSearchModel(contentTitle: "농촌 봉사활동", startAt: "20221124", endAt: "20221231", isLiked: true)
+]
+
