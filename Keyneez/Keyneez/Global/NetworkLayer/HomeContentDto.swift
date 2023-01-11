834c032518357c0cc692c24e2ae6f0b4910a7b6f
kpk0616 <kpk0616@naver.com>
2023-01-11 17:44:12 +0900

    [Add] #41 - HomeContentDto 추가

---
+++ Keyneez/Keyneez/Global/NetworkLayer/HomeContentDto.swift
@@ -0,0 +1,13 @@
+//
+//  HomeContentDto.swift
+//  Keyneez
+//
+//  Created by 박의서 on 2023/01/11.
+//
+
+import Foundation
+
+struct HomeContentDto: Codable {
+  let status: Int
+  let message: String
+}
