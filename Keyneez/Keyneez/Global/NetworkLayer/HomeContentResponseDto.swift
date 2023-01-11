ded8115da9e421fde4cd63ab41dbe2c6ff4a13a6
kpk0616 <kpk0616@naver.com>
2023-01-11 17:46:14 +0900

    [Rename] #41 - HomeContentDto 이름변경

---
+++ Keyneez/Keyneez/Global/NetworkLayer/HomeContentResponseDto.swift
@@ -0,0 +1,18 @@
+//
+//  HomeContentResponseDto.swift
+//  Keyneez
+//
+//  Created by 박의서 on 2023/01/11.
+//
+
+import Foundation
+
+struct HomeContentResponseDto: Codable {
+  let status: Int
+  let message: String
+  let data: [HomeContentResponseData]
+}
+
+struct HomeContentResponseData: Codable {
+  
+}
