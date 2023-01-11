628bdaca7908b6cbce67c08b3ea38227d02df54c
kpk0616 <kpk0616@naver.com>
2023-01-11 17:52:28 +0900

    [Chore] #41 - HomeContentResponseDto 작성

--- Keyneez/Keyneez/Global/NetworkLayer/HomeContentResponseDto.swift
+++ Keyneez/Keyneez/Global/NetworkLayer/HomeContentResponseDto.swift
@@ -14,5 +14,13 @@ struct HomeContentResponseDto: Codable {
 }
 
 struct HomeContentResponseData: Codable {
-  
+  let contentKey: Int
+  let contentTitle,
+      contentImg,
+      introduction,
+      usage,
+      startAt,
+      endAt: String
+  let liked: Bool
+  let category: [String]
 }
