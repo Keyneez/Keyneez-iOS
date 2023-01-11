f195f5cfd562b0c9af99fbaa455f2f83fd90bbaa
Dohyun Chung <pastapeter@naver.com>
2023-01-11 21:44:19 +0900

    [Chore] Lint Style

--- Keyneez/Keyneez/Tab/ID/IDViewController.swift
+++ Keyneez/Keyneez/Tab/ID/IDViewController.swift
@@ -32,7 +32,6 @@ final class IDViewController: NiblessViewController, NavigationBarProtocol {
     super.viewDidLoad()
     addNavigationViewToSubview()
     setBackgroundColorToBlack()
-  }
     
 }
 
